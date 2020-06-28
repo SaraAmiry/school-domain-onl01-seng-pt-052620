91356f6bc9797f8745089eb1a0eb891711fe615d
Sara Amiry <saraamiry24@gmail.com>
2020-06-28 04:17:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @roster.each {
+      if grade != grade 
+        @roster[grade] = [name]
+      elsif grade == grade
+        @roster = [name]
+      else
+        @roster
+      end 
+    }
+  end
+end 
