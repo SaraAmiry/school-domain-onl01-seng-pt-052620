be5ef9f3e6b496fd3e24bdd085eae719dba46eaf
Sara Amiry <saraamiry24@gmail.com>
2020-06-28 04:19:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,14 +6,6 @@ class School
   end
   
   def add_student(name, grade)
-    @roster.each {
-      if grade != grade 
-        @roster[grade] = [name]
-      elsif grade == grade
-        @roster = [name]
-      else
-        @roster
-      end 
-    }
+    self.roster[grade] << name
   end
 end 
