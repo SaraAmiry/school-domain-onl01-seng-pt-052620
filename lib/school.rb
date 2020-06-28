9c72aecdd1e54cbf00cd884a6baec9b663c40301
Sara Amiry <saraamiry24@gmail.com>
2020-06-28 04:18:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,7 +2,7 @@ class School
   attr_accessor :name, :roster
   def initialize(name)
     @name = name
-    @roster = {}
+    @roster = Hash.new {|h, k| h[k] = []}
   end
   
   def add_student(name, grade)
